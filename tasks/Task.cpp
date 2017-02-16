2b25b626ab33e3b81eb8f0f02971adb764eaff9f
pagnamen <fjalar@esa.int>
2017-02-16 12:29:38 +0100

    Added saving of distance image

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -52,27 +52,29 @@ void Task::updateHook()
 
 	if(_distance_frame.read(distance_image) == RTT::NewData)
 	{
-		std::cout << "checkpoint" << std::endl;
+		myDEM.saveDistanceFrame(distance_image.data);
+		_distance_frame_path.write(myDEM.getDistanceFramePath());
+		
 		_left_frame_rect.read(leftFrame);
 		cv::Mat dst = frame_helper::FrameHelper::convertToCvMat(*leftFrame);
-                cv::Mat dst2 = dst;
-                if ( (camera_name=="FLOC_STEREO") || (camera_name=="RLOC_STEREP") )
-                {
-                    _right_frame_rect.read(rightFrame);
-                    dst2 = frame_helper::FrameHelper::convertToCvMat(*leftFrame);
-                }
+		cv::Mat dst2 = dst;
+		if ( (camera_name=="FLOC_STEREO") || (camera_name=="RLOC_STEREP") )
+		{
+			_right_frame_rect.read(rightFrame);
+			dst2 = frame_helper::FrameHelper::convertToCvMat(*leftFrame);
+		}
 		myDEM.setColorFrame(dst, dst2); // to opencv format. Basically set up so that first it is read to internal variable, then converted to opencv and sent over to my library
 		myDEM.distance2pointCloud(distance_image.data);
-		
+
 		myDEM.pointCloud2Mesh();
-		
+
 		_mesh_path.write(myDEM.getMeshPath());
 		_image_left_path.write(myDEM.getImageLeftPath());
-                 if ( (camera_name=="FLOC_STEREO") || (camera_name=="RLOC_STEREO") )
-                {
-		    _image_right_path.write(myDEM.getImageRightPath());
-                }
-
+		
+		if ( (camera_name=="FLOC_STEREO") || (camera_name=="RLOC_STEREO") )
+		{
+			_image_right_path.write(myDEM.getImageRightPath());
+		}
 	}
     
 }
